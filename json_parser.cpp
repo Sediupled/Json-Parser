988ca15a110794aa729bef2643f1d89e46b4bf52
Chaitanya <chaitanya.rook@gmail.com>
2026-08-25 01:27:28 -0700

    can parse multiline files with values

--- json_parser.cpp
+++ json_parser.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <variant>
-#include <memory>
 #include <unordered_map>
 #include <map>
 #include <vector>
+#include <fstream>
+#include <memory>
 
 /*Token Types*/
 enum TokenType{
@@ -28,6 +29,7 @@ class Token{
         TokenType t_type;
         V t_val;
 
+        Token(){}
         Token(TokenType type,V val) {
             t_type = type;
             t_val = val;
@@ -44,7 +46,8 @@ class Token{
 /*Json Structured Types, ts might be jank not sure yet*/
 struct JsonObj;
 struct JsonArr;
-using JsonVal = std::variant<std::string, std::unique_ptr<JsonObj>, int, double, std::unique_ptr<JsonArr>>;
+/*using JsonVal = std::variant<std::unique_ptr<std::string>, std::unique_ptr<JsonObj>, std::unique_ptr<int>, std::unique_ptr<double>, std::unique_ptr<JsonArr>>;*/
+using JsonVal = std::variant<std::string, double, int>;
 
 struct JsonObj{
     std::string name;
@@ -69,15 +72,15 @@ struct JsonContents{
 /*---------------------------------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------------*/
 
-template<typename V>
 class Interpreter{
     public:
         int pos;
-        char curChar;
+        std::string curChar;
         std::string text;
         JsonContents parsed;
-        Interpreter(){
+        Interpreter(std::string textFromFile){
             pos = 0;
+            text = textFromFile;
             curChar = text[pos];
         }
         ~Interpreter(){}
@@ -87,7 +90,36 @@ class Interpreter{
         void advance() {
         }
 
-        void getNextToken(){
+        template<typename V>
+        Token<V> getNextToken(){
+            Token<V> tok;
+            if (curChar == "\""){
+                tok = Token<V>(STRING,processString());
+            }
+            else if (curChar[0] >= '0' && curChar[0] <= '9' || curChar[0] == '.'){
+                std::string pnumstr= processNumber();
+
+                if (pnumstr.contains(".")){
+                    double pnum = std::stod(pnumstr);
+                    tok = Token<V>(NUMBER,pnum);
+                }
+                else{
+                    int pnum = std::stoi(pnumstr);
+                    tok = Token<V>(NUMBER,pnum);
+                }
+            }
+            else if(curChar[0] == '\n'){
+                pos++;
+                curChar = text[pos];
+                tok = getNextToken<V>();
+            }
+            else if (pos>=text.length()){
+                tok = Token<V>(EOFTYPE,curChar);
+                return tok;
+            }
+            pos++;
+            curChar = text[pos];
+            return tok;
         }
 
         void eat(){}
@@ -96,23 +128,45 @@ class Interpreter{
         /*Parser*/
         void createObj(){}
         void createArray(){}
-        void processNumber(){}
-        void processString(TokenType strType){}
 
+        std::string processNumber(){
+            std::string numStr;
+            while(curChar[0] >= '0' && curChar[0] <= '9' || curChar[0] == '.'||curChar[0] == 'E'||curChar[0] == 'e'){
+                numStr += curChar;
+                curChar = text[++pos];
+            }
+            pos--;
+            numStr.erase(std::remove(numStr.begin(), numStr.end(), '\n'), numStr.end());
+            return numStr;
+        }
+
+/*Returns with pos still on last character of string, generally "*/
+        std::string processString(){
+            std::string retstr;
+            retstr += curChar;
+            curChar = text[++pos];
+            while(curChar!= "\""){
+                retstr+= curChar;
+                curChar = text[++pos];
+            }
+
+            retstr +=curChar;
+
+            return retstr;
+        }
+
+        template<typename V>
         void expr(){
-            Token<V> curToken;
+            Token<V> curToken = getNextToken<V>();
 
             while(curToken.t_type != EOFTYPE){
-                curToken = getNextToken();
-                if (curToken.t_type == STRING){
-                    parsed.contents.push_back(curToken.t_val);
-                }
+                parsed.contents.push_back(curToken.t_val);
+                curToken = getNextToken<V>();
             }
         }
 
         void deJSONify()
         {
-            this->expr();
             for(auto& val : parsed.contents)
             {
                 std::visit([](auto&& v){std::cout << v << std::endl;}, val);
@@ -129,4 +183,24 @@ class Interpreter{
 /*---------------------------------------------------------------------------------------------------*/
 
 int main(){
+    std::fstream jsonFile;
+    jsonFile.open("test.json", std::ios::in);
+
+    std::string textFromFile;
+    std::string line;
+
+    if (jsonFile.is_open()){
+        while( getline(jsonFile, line))
+        {
+            textFromFile += line + '\n';
+        }
+        /*for(auto v: textFromFile){*/
+        /*    std::cout << v << std::endl;*/
+        /*}*/
+    }
+
+    Interpreter interp(textFromFile);
+    interp.expr<JsonVal>();
+    interp.deJSONify();
+
 }
